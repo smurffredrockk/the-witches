ebfeddc318ce1edb93f5f206013247c28fced1aa
smurffredrockk <67315398+smurffredrockk@users.noreply.github.com>
2020-06-24 12:51:42 +0100

    Updates

---
+++ precache-manifest.d491e56d802b8fbefc9e2cf46f8c5a0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b03f2c9487709562fefd83a789770c",
+    "url": "/the-witches/index.html"
+  },
+  {
+    "revision": "c2f21a46552b51ead651",
+    "url": "/the-witches/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfaff660c734c9651e78",
+    "url": "/the-witches/static/js/2.f7af909e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-witches/static/js/2.f7af909e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f21a46552b51ead651",
+    "url": "/the-witches/static/js/main.d85ffcc7.chunk.js"
+  },
+  {
+    "revision": "8a5cfab174ddeacc3e1e",
+    "url": "/the-witches/static/js/runtime-main.5c5a2d3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-witches/static/media/logo.5d5d9eef.svg"
+  }
+]);
